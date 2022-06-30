a1e8c250348988cbb602d1fcac41c1ae0258b559
Philippe Lamorski <9007452-philou62@users.noreply.gitlab.com>
2022-06-30 16:40:50 +0200

    Ajout du module product-card et affichage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { ProductsPageComponent } from './_pages/products-page/products-page.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './_pages/home-page/home-page.component';
+import { NotFoundComponent } from './_pages/not-found/not-found.component';
 
 const routes: Routes = [
-  
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomePageComponent },
+  { path: 'products', component: ProductsPageComponent },
+  { path: '**', component: NotFoundComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FooterComponent } from './_commons/footer/footer.component';
 import { NotFoundComponent } from './_pages/not-found/not-found.component';
 import { HomePageComponent } from './_pages/home-page/home-page.component';
 import { ProductsPageComponent } from './_pages/products-page/products-page.component';
+import { ProductListComponent } from './_pages/products-page/product-list/product-list.component';
+import { ProductCardComponent } from './_pages/products-page/product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { ProductsPageComponent } from './_pages/products-page/products-page.comp
     FooterComponent,
     NotFoundComponent,
     HomePageComponent,
-    ProductsPageComponent
+    ProductsPageComponent,
+    ProductListComponent,
+    ProductCardComponent,
   ],
   imports: [
     BrowserModule,
