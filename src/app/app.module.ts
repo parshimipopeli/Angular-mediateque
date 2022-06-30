c62c3a9d66a9e73932f15fd41ecaa725bffcbef2
Philippe Lamorski <9007452-philou62@users.noreply.gitlab.com>
2022-06-30 11:51:21 +0200

    Ajout du fichier README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './_commons/nav-bar/nav-bar.component';
+import { FooterComponent } from './_commons/footer/footer.component';
+import { NotFoundComponent } from './_pages/not-found/not-found.component';
+import { HomePageComponent } from './_pages/home-page/home-page.component';
+import { ProductsPageComponent } from './_pages/products-page/products-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HomePageComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule,
